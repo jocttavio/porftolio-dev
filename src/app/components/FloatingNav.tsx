69bafe1f4bc80ad526aaca7186b0956e41b0a9d0
jocttavio <josp1601@gmail.com>
2025-08-14 17:55:12 -0600

    Add styles to create a navbar with border color dymanic

--- src/app/components/FloatingNav.tsx
+++ src/app/components/FloatingNav.tsx
@@ -7,7 +7,7 @@ import {
   useMotionValueEvent,
 } from "framer-motion";
 import Link from "next/link";
-// import { cn } from "@/lib/utils";
+import styles from '../css/navbar.module.css'
 import { FaRegSun, FaMoon } from "react-icons/fa";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 type NavItem = {
@@ -83,7 +83,7 @@ export const FloatingNav = ({ navItems }: { navItems: NavItem[] }) => {
         transition={{
           duration: 0.2,
         }}
-        className="flex dark:border-white/[0.2] dark:bg-[#111928bf] bg-white border-transparent max-w-fit min-w-[300px] md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4 text-lg"
+        className={`flex dark:border-white/[0.2] dark:bg-[#111928bf] bg-white border-transparent max-w-fit min-w-[300px] md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4 text-lg ${styles.navbar}`}
         style={{
           backdropFilter: "blur(16px) saturate(180%)",
           borderRadius: "12px",
